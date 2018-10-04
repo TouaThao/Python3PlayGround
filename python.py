5f49bca81f38e6dc2e616c17603e10c65963e078
Toua Thao <touathao09@gmail.com>
2018-10-04 15:05:12 -0500

    testing

---
+++ python.py
@@ -0,0 +1,13 @@
+"""This is hello world"""
+print("Hello World")
+
+"""This is my first shape"""
+print("|---/")
+print("|  /")
+print("| /")
+print("|/")
+
+print("There once was a man name George,")
+print("He was 70 years old")
+print("He really like the name George,")
+print("But he didn't like being 70")

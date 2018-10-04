2fed24fcc5a3ad910ad96723f40e47be3fc14c59
Toua Thao <touathao09@gmail.com>
2018-10-04 16:31:18 -0500

    changing name and age

--- python.py
+++ python.py
@@ -1,13 +1,19 @@
-"""This is hello world"""
+"This is hello world"
 print("Hello World")
 
-"""This is my first shape"""
+"This is my first shape"
 print("|---/")
 print("|  /")
 print("| /")
 print("|/")
 
-print("There once was a man name George,")
-print("He was 70 years old")
-print("He really like the name George,")
-print("But he didn't like being 70")
+" This is to change character name and age with a variable "
+"note: This allow us to change character name and age just with the variable instead of going line by line"
+"note: this will save time and effort"
+character_name = "Jonny"
+character_age = "19"
+print("There once was a man name " + character_name +",")
+print("He was "+ character_age +  " years old")
+print("He really like the name "+ character_name +",")
+print("But he didn't like being "+ character_age)
+
